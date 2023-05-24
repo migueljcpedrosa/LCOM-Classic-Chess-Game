7cede77c7a6e6a7fed69b6c864a559398cd19b69
Miguel Pedrosa <93871576+migueljcpedrosa@users.noreply.github.com>
2023-05-24 18:52:19 +0100

    Defined rtc_timestamp struct

--- proj/src/drivers/rtc/rtc.h
+++ proj/src/drivers/rtc/rtc.h
@@ -4,6 +4,13 @@
 #include <minix/sysutil.h>
 #include <lcom/lcf.h>
 
-
+typedef struct {
+    uint8_t year;
+    uint8_t month;
+    uint8_t day;
+    uint8_t hours;
+    uint8_t minutes;
+    uint8_t seconds;
+} rtc_timestamp;
 
 #endif
