905e5cf1c563d2715875a35f871d115f132ad3dc
Miguel Pedrosa <93871576+migueljcpedrosa@users.noreply.github.com>
2023-05-24 19:07:50 +0100

    Defined RTC_COMMAND_REGISTER and RTC_DATA_REGISTER macros

--- proj/src/drivers/rtc/rtc.c
+++ proj/src/drivers/rtc/rtc.c
@@ -1,3 +1,7 @@
 #include "rtc.h"
 
 int hook_id = 8;
+int irq_rtc = 8;
+
+rtc_timestamp_t rtc_timestamp;
+bool rtc_binary_mode;
--- proj/src/drivers/rtc/rtc.h
+++ proj/src/drivers/rtc/rtc.h
@@ -3,6 +3,10 @@
 
 #include <minix/sysutil.h>
 #include <lcom/lcf.h>
+#include <stdbool.h>
+
+#define RTC_COMMAND_REGISTER 0x70
+#define RTC_DATA_REGISTER    0x71
 
 typedef struct {
     uint8_t year;
@@ -11,6 +15,6 @@ typedef struct {
     uint8_t hours;
     uint8_t minutes;
     uint8_t seconds;
-} rtc_timestamp;
+} rtc_timestamp_t;
 
 #endif
