444d9a0223d1a266036d9e2b9b3a33353a8407d5
Ramos <1joaoramos1@gmail.com>
2023-05-26 22:08:34 +0100

    game_util

---
+++ proj/src/model/game/game_util.h
@@ -0,0 +1,41 @@
+#ifndef _GAME_UTIL_H_
+#define _GAME_UTIL_H_
+
+#define ROUND_TIME 600
+
+typedef struct
+{
+    int x;
+    int y;
+} Position;
+
+typedef enum {
+    WHITE,
+    BLACK,
+} Color;
+
+typedef enum {
+    EMPTY,
+    PAWN,
+    KNIGHT,
+    ROOK,
+    BISHOP,
+    QUEEN,
+    KING
+} Type;
+
+typedef enum {
+    NORMAL,
+    CAPTURE,
+    CASTLE,
+    EN_PASSANT
+} MoveType;
+
+typedef struct {
+    Position origin;
+    Position destination;
+    MoveType type;
+
+} Move;
+
+#endif
